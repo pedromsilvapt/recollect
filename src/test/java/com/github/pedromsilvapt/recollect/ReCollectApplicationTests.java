35e3e32fdcc67ff77dd01f72e33e2bc3ca598347
Pedro M. Silva <pemiolsi@hotmail.com>
2023-10-24 21:19:34 +0100

    chore: initial commit

---
+++ src/test/java/com/github/pedromsilvapt/recollect/ReCollectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.pedromsilvapt.recollect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReCollectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
